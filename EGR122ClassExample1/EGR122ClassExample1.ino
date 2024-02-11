b46c56f16b0b75402a0e90198918a96de5322502
professorwade <schofieldw@centralvirginia.edu>
2024-02-11 11:51:48 -0500

    Initial Commit of EGR 122 Example

---
+++ EGR122ClassExample1/EGR122ClassExample1.ino
@@ -0,0 +1,46 @@
+#include <Servo.h>
+
+// C++ code
+//
+
+Servo servo1;
+
+int motorControl = 7;
+int motorSwitch = 11;
+int servoControl = 2;
+int potRead = A0;
+int ledControl = 5;
+int armMemory = 0;
+
+void setup()
+{
+  pinMode(motorControl, OUTPUT);
+  pinMode(motorSwitch, INPUT_PULLUP);
+  pinMode(ledControl, OUTPUT);
+  servo1.attach(servoControl);  
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  
+  // Control Motor On / Off
+  bool val = digitalRead(motorSwitch);
+  if (val) {
+    digitalWrite(ledControl, HIGH); // OFF
+  	digitalWrite(motorControl, LOW);  
+  }
+  else {
+    digitalWrite(ledControl, LOW); // ON
+    digitalWrite(motorControl, HIGH);
+  }
+  
+  // Control Servo Position
+  int arm = analogRead(potRead);
+  arm = map(arm, 0, 1023, 0, 255); 
+  servo1.write(arm); 
+  Serial.println(arm);
+}
+
+
+
