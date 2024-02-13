1cc3e53c9e46d04740c01b0022b2413ead17da5d
professorwade <schofieldw@centralvirginia.edu>
2024-02-12 19:47:55 -0500

    Update EGR122ClassExample1.ino
    Attempt to filter noise digitally

--- EGR122ClassExample1/EGR122ClassExample1.ino
+++ EGR122ClassExample1/EGR122ClassExample1.ino
@@ -10,7 +10,7 @@ int motorSwitch = 11;
 int servoControl = 2;
 int potRead = A0;
 int ledControl = 5;
-int armMemory = 0;
+long int ms100;
 
 void setup()
 {
@@ -19,6 +19,7 @@ void setup()
   pinMode(ledControl, OUTPUT);
   servo1.attach(servoControl);  
   Serial.begin(9600);
+  ms100 = millis();
 }
 
 void loop()
@@ -35,11 +36,16 @@ void loop()
     digitalWrite(motorControl, HIGH);
   }
   
-  // Control Servo Position
-  int arm = analogRead(potRead);
-  arm = map(arm, 0, 1023, 0, 255); 
-  servo1.write(arm); 
-  Serial.println(arm);
+  // Control Servo Position (every 100ms)
+  if (millis() - ms100 > 200) {
+    int arm = analogRead(potRead);
+    arm = map(arm, 0, 1023, 0, 255); 
+    servo1.write(arm); 
+    Serial.println(arm);
+    ms100 = millis();
+  }
+
+
 }
 
 
